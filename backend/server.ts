cdf65b4c248caf7e591cb08dced16692b2d40913
Sheikh Umair <umair.najeeb@iquasar.com>
2024-02-08 19:08:48 +0530

    Downloaded Cors

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import connectToDb from "./config/connectToDb";
+import cors from "cors"
 
 configDotenv(); 
 const app = express(); // Creating a server
 connectToDb(); //Connecting to database
+app.use(express.json())
+app.use(cors())
+
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on PORT", process.env.PORT);
